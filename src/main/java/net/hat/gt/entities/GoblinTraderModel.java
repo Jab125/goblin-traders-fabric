939f1f2211092e545129765450a0a7ddeda045dd
hat <flint9877@gmail.com>
2021-09-29 12:13:37 +0800

    Fixed No-leggy-mc-gee, changed animation stuff

--- src/main/java/net/hat/gt/entities/GoblinTraderModel.java
+++ src/main/java/net/hat/gt/entities/GoblinTraderModel.java
@@ -5,53 +5,67 @@ import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.model.*;
 import net.minecraft.client.render.entity.model.CompositeEntityModel;
+import net.minecraft.client.render.entity.model.EntityModelPartNames;
 
 @Environment(EnvType.CLIENT)
                             //DON'T DELETE G!!!
                             //WE NEED THE G - backup - <G extends AbstractGoblinEntity>
 public class GoblinTraderModel<G extends AbstractGoblinEntity> extends CompositeEntityModel<AbstractGoblinEntity> {
+    private final ModelPart root;
     private final ModelPart head;
+    public final ModelPart hood;
     private final ModelPart body;
     private final ModelPart leftEar;
     private final ModelPart rightEar;
-    private final ModelPart leftArm;
     private final ModelPart rightArm;
+    private final ModelPart leftArm;
     private final ModelPart leftLeg;
     private final ModelPart rightLeg;
+    private final ModelPart nose;
+    private final ModelPart bag;
 
     public GoblinTraderModel(ModelPart root) {
-        this.head = root.getChild("head");
+        this.root = root;
+        this.head = root.getChild(EntityModelPartNames.HEAD);
+        this.hood = root.getChild("hood");
+        this.nose = this.head.getChild("nose");
         this.rightEar = this.head.getChild("rightEar");
         this.leftEar = this.head.getChild("leftEar");
-        this.body = root.getChild("body");
-        this.rightArm = root.getChild("rightArm");
-        this.leftArm = root.getChild("leftArm");
-        this.leftLeg = root.getChild("leftLeg");
-        this.rightLeg = root.getChild("rightLeg");
+        this.body = root.getChild(EntityModelPartNames.BODY);
+        this.bag = this.body.getChild("bag");
+        this.rightArm = root.getChild(EntityModelPartNames.RIGHT_ARM);
+        this.leftArm = root.getChild(EntityModelPartNames.LEFT_ARM);
+        this.leftLeg = root.getChild(EntityModelPartNames.LEFT_LEG);
+        this.rightLeg = root.getChild(EntityModelPartNames.RIGHT_LEG);
     }
 
     public static TexturedModelData getTexturedModelData() {
         ModelData modelData = new ModelData();
-        ModelPartData modelPartData = modelData.getRoot();
-        modelPartData.addChild("body", ModelPartBuilder.create().uv(0,20).cuboid(-2.5F, -9.5F, 2.0F, 5.0F, 7.0F, 3.0F).uv(12,12).cuboid(-2.0F, -8.0F, -2.0F, 4.0F, 4.0F, 4.0F), ModelTransform.pivot(0.0F,24.0F,0.0F));
-        ModelPartData modelPartData1 = modelPartData.addChild("head", ModelPartBuilder.create().uv(0,0).cuboid(-4.0F, -14.0F, -3.0F, 8.0F, 6.0F, 6.0F).uv(0,32).cuboid(-4.0F, -14.0F, -3.0F, 8.0F, 8.0F, 6.0F, new Dilation(0.5F)).uv(22,0).cuboid(-1.0F, -11.0F, -5.0F, 2.0F, 4.0F, 2.0F), ModelTransform.pivot(0.0F,24.0F,0.0F));
-        modelPartData1.addChild("leftEar", ModelPartBuilder.create().uv(0,8).cuboid(2.0F, -13.0F, 3.0F, 0.0F, 4.0F, 4.0F), ModelTransform.pivot(0.0F,0.0F,0.0F));
-        modelPartData1.addChild("rightEar", ModelPartBuilder.create().uv(8,8).cuboid(-2.0F, -13.0F, 3.0F, 0.0F, 4.0F, 4.0F), ModelTransform.pivot(0.0F,0.0F,0.0F));
-        modelPartData.addChild("rightArm", ModelPartBuilder.create().uv(30,0).cuboid(-4.0F, -8.0F, -1.0F, 2.0F, 5.0F, 2.0F), ModelTransform.pivot(0.0F,24.0F,0.0F));
-        modelPartData.addChild("leftArm", ModelPartBuilder.create().uv(38,0).cuboid(2.0F, -8.0F, -1.0F, 2.0F, 5.0F, 2.0F), ModelTransform.pivot(0.0F,24.0F,0.0F));
-        modelPartData.addChild("leftLeg", ModelPartBuilder.create().uv(36,9).cuboid(0.0F, -4.0F, -1.0F, 2.0F, 4.0F, 3.0F), ModelTransform.pivot(0.0F,24.0F,0.0F));
-        modelPartData.addChild("rightLeg", ModelPartBuilder.create().uv(26,9).cuboid(-2.0F, -4.0F, -1.0F, 2.0F, 4.0F, 3.0F), ModelTransform.pivot(0.0F,24.0F,0.0F));
+        ModelPartData root = modelData.getRoot();
+        ModelPartData body = root.addChild(EntityModelPartNames.BODY, ModelPartBuilder.create().uv(12,12).cuboid(-2.0F, 0.0F, -2.0F, 4.0F, 4.0F, 4.0F), ModelTransform.pivot(0.0F, 16.0F, 0.0F));
+        body.addChild("bag", ModelPartBuilder.create().uv(0,20).cuboid(-2.5F, -2.0F, 2.0F, 5.0F, 7.0F, 3.0F), ModelTransform.NONE);
+        ModelPartData head = root.addChild(EntityModelPartNames.HEAD, ModelPartBuilder.create().uv(0,0).cuboid(-4.0F, -6.0F, -3.0F, 8.0F, 6.0F, 6.0F), ModelTransform.pivot(0.0F, 16.0F, 0.0F));
+        head.addChild("leftEar", ModelPartBuilder.create().uv(0,8).cuboid(0.0F, -2.0F, 0.0F, 0.0F, 4.0F, 4.0F), ModelTransform.pivot(4.0F, -3.0F, 1.0F));
+        head.addChild("rightEar", ModelPartBuilder.create().uv(8,8).cuboid(0.0F, -2.0F, 0.0F, 0.0F, 4.0F, 4.0F), ModelTransform.pivot(-4.0F, -3.0F, 1.0F));
+        head.addChild("nose", ModelPartBuilder.create().uv(22, 0).cuboid(-1.0F, 0.0F, -2.0F, 2.0F, 4.0F, 2.0F), ModelTransform.pivot(0.0F, -3.0F, -3.0F));
+        root.addChild(EntityModelPartNames.RIGHT_ARM, ModelPartBuilder.create().uv(30,0).cuboid(-1.0F, -1.0F, -1.0F, 2.0F, 5.0F, 2.0F), ModelTransform.pivot(-3.0F, 17.0F, 0.0F));
+        root.addChild(EntityModelPartNames.LEFT_ARM, ModelPartBuilder.create().uv(38,0).cuboid(-1.0F, -1.0F, -1.0F, 2.0F, 5.0F, 2.0F), ModelTransform.pivot(3.0F, 17.0F, 0.0F));
+        root.addChild(EntityModelPartNames.LEFT_LEG, ModelPartBuilder.create().uv(36,9).cuboid(-1.0F, 0.0F, -1.5F, 2.0F, 4.0F, 3.0F), ModelTransform.pivot(1.0F, 20.0F, 0.0F));
+        root.addChild(EntityModelPartNames.RIGHT_LEG, ModelPartBuilder.create().uv(26,9).cuboid(-1.0F, 0.0F, -1.5F, 2.0F, 4.0F, 3.0F), ModelTransform.pivot(-1.0F, 20.0F, 0.0F));
+        root.addChild("hood", ModelPartBuilder.create().uv(0, 32).cuboid(-4.0F, -6.0F, -3.0F, 8.0F, 8.0F, 6.0F, new Dilation(0.5F)), ModelTransform.pivot(0.0F, 16.0F, 0.0F));
         return TexturedModelData.of(modelData,46,46);
-        //Redo Model
     }
 
-
+    @Override
+    public Iterable<ModelPart> getParts() {
+        return ImmutableList.of(this.root);
+    }
 
     @Override
-    public void setAngles(AbstractGoblinEntity entity, float limbAngle, float limbDistance, float animationProgress, float headYaw, float headPitch) {
+    public void setAngles(AbstractGoblinEntity entity, float limbAngle, float limbDistance, float animationProgress, float headYaw, float headroll) {
         this.leftEar.yaw = 0.7854F;
         this.rightEar.yaw = -0.7854F;
-        /*float rotateFactor;
+        float rotateFactor;
         rotateFactor = (float) entity.getRotationVector().lengthSquared();
         rotateFactor = rotateFactor / 0.2F;
         rotateFactor = rotateFactor * rotateFactor * rotateFactor;
@@ -59,36 +73,32 @@ public class GoblinTraderModel<G extends AbstractGoblinEntity> extends Composite
         {
             rotateFactor = 1.0F;
         }
-        this.rightArm.pivotX = (float) (Math.cos(limbAngle * 0.6662F + (float) Math.PI) * 2.0F * limbDistance * 0.5F / rotateFactor);
-        this.leftArm.pivotX = (float) (Math.cos(limbAngle * 0.6662F) * 2.0F * limbDistance * 0.5F / rotateFactor);
-        this.rightArm.pivotY = 0.0F;
-        this.rightArm.pivotZ = 0.0F;
-        this.leftArm.pivotY = 0.0F;
-        this.leftArm.pivotZ = 0.0F;
-        this.rightLeg.pivotX = (float) (Math.cos(limbAngle * 0.6662F) * 1.4F * limbDistance / rotateFactor);
-        this.leftLeg.pivotX = (float) (Math.cos(limbAngle * 0.6662F + (float) Math.PI) * 1.4F * limbDistance / rotateFactor);
-        this.head.pivotY = headYaw * ((float) Math.PI / 180F);
-        this.head.pivotX = headPitch * ((float) Math.PI / 180F);
+        this.rightArm.pitch = (float) (Math.cos(limbAngle * 0.6662F + (float) Math.PI) * 2.0F * limbDistance * 0.5F / rotateFactor);
+        this.leftArm.pitch = (float) (Math.cos(limbAngle * 0.6662F) * 2.0F * limbDistance * 0.5F / rotateFactor);
+        this.rightArm.yaw = 0.0F;
+        this.rightArm.roll = 0.0F;
+        this.leftArm.yaw = 0.0F;
+        this.leftArm.roll = 0.0F;
+        this.rightLeg.pitch = (float) (Math.cos(limbAngle * 0.6662F) * 1.4F * limbDistance / rotateFactor);
+        this.leftLeg.pitch = (float) (Math.cos(limbAngle * 0.6662F + (float) Math.PI) * 1.4F * limbDistance / rotateFactor);
+        this.head.yaw = headYaw * ((float) Math.PI / 180F);
+        this.head.pitch = headroll * ((float) Math.PI / 180F);
+        this.hood.copyTransform(this.head);
 
         if(entity.isUsingItem())
         {
             double rotateX = Math.toRadians(-90F + 5F * Math.sin(animationProgress));
-            this.rightArm.pivotX = (float) rotateX;
-            this.leftArm.pivotX = (float) rotateX;
-            this.rightArm.pivotX = (float) Math.toRadians(-90F);
-            this.rightLeg.pivotY = (float) Math.toRadians(25F);
-            this.leftLeg.pivotX = (float) Math.toRadians(-90F);
-            this.leftLeg.pivotY = (float) Math.toRadians(-25F);
+            this.rightArm.pitch = (float) rotateX;
+            this.leftArm.pitch = (float) rotateX;
+            this.rightArm.pitch = (float) Math.toRadians(-90F);
+            this.rightLeg.yaw = (float) Math.toRadians(25F);
+            this.leftLeg.pitch = (float) Math.toRadians(-90F);
+            this.leftLeg.yaw = (float) Math.toRadians(-25F);
         }
         else
         {
-            this.rightLeg.pivotY = (float) Math.toRadians(0);
-            this.leftLeg.pivotY = (float) Math.toRadians(0);
-        }*/
-    }
-
-    @Override
-    public Iterable<ModelPart> getParts() {
-        return ImmutableList.of(this.head, this.body);
+            this.rightLeg.yaw = (float) Math.toRadians(0);
+            this.leftLeg.yaw = (float) Math.toRadians(0);
+        }
     }
 }
