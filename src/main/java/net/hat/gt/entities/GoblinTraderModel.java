47d62ade0b4d8709fde9890666b87cfc50722b20
hat <flint9877@gmail.com>
2021-09-29 13:29:23 +0800

    Animations Added

--- src/main/java/net/hat/gt/entities/GoblinTraderModel.java
+++ src/main/java/net/hat/gt/entities/GoblinTraderModel.java
@@ -6,11 +6,16 @@ import net.fabricmc.api.Environment;
 import net.minecraft.client.model.*;
 import net.minecraft.client.render.entity.model.CompositeEntityModel;
 import net.minecraft.client.render.entity.model.EntityModelPartNames;
+import net.minecraft.client.render.entity.model.ModelWithArms;
+import net.minecraft.client.render.entity.model.ModelWithHead;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.util.Arm;
+import net.minecraft.util.math.MathHelper;
 
 @Environment(EnvType.CLIENT)
                             //DON'T DELETE G!!!
                             //WE NEED THE G - backup - <G extends AbstractGoblinEntity>
-public class GoblinTraderModel<G extends AbstractGoblinEntity> extends CompositeEntityModel<AbstractGoblinEntity> {
+public class GoblinTraderModel<G extends AbstractGoblinEntity> extends CompositeEntityModel<AbstractGoblinEntity> implements ModelWithArms, ModelWithHead {
     private final ModelPart root;
     private final ModelPart head;
     public final ModelPart hood;
@@ -66,24 +71,43 @@ public class GoblinTraderModel<G extends AbstractGoblinEntity> extends Composite
         this.leftEar.yaw = 0.7854F;
         this.rightEar.yaw = -0.7854F;
         float rotateFactor;
-        rotateFactor = (float) entity.getRotationVector().lengthSquared();
+        rotateFactor = (float) entity.getVelocity().lengthSquared();
         rotateFactor = rotateFactor / 0.2F;
         rotateFactor = rotateFactor * rotateFactor * rotateFactor;
         if(rotateFactor < 1.0F)
         {
             rotateFactor = 1.0F;
         }
-        this.rightArm.pitch = (float) (Math.cos(limbAngle * 0.6662F + (float) Math.PI) * 2.0F * limbDistance * 0.5F / rotateFactor);
-        this.leftArm.pitch = (float) (Math.cos(limbAngle * 0.6662F) * 2.0F * limbDistance * 0.5F / rotateFactor);
+        this.rightArm.pitch = MathHelper.cos(limbAngle * 0.6662F + (float) Math.PI) * 2.0F * limbDistance * 0.5F / rotateFactor;
+        this.leftArm.pitch = MathHelper.cos(limbAngle * 0.6662F) * 2.0F * limbDistance * 0.5F / rotateFactor;
         this.rightArm.yaw = 0.0F;
         this.rightArm.roll = 0.0F;
         this.leftArm.yaw = 0.0F;
         this.leftArm.roll = 0.0F;
-        this.rightLeg.pitch = (float) (Math.cos(limbAngle * 0.6662F) * 1.4F * limbDistance / rotateFactor);
-        this.leftLeg.pitch = (float) (Math.cos(limbAngle * 0.6662F + (float) Math.PI) * 1.4F * limbDistance / rotateFactor);
+        this.rightLeg.pitch = MathHelper.cos(limbAngle * 0.6662F) * 1.4F * limbDistance / rotateFactor;
+        this.leftLeg.pitch = MathHelper.cos(limbAngle * 0.6662F + (float) Math.PI) * 1.4F * limbDistance / rotateFactor;
         this.head.yaw = headYaw * ((float) Math.PI / 180F);
         this.head.pitch = headroll * ((float) Math.PI / 180F);
         this.hood.copyTransform(this.head);
+
+        if(this.handSwingProgress > 0.0F)
+        {
+            ModelPart arm = this.rightArm;
+            float progress = this.handSwingProgress;
+            this.body.yaw = MathHelper.sin(MathHelper.sqrt(progress) * ((float) Math.PI * 2F)) * 0.2F;
+            this.rightArm.yaw += this.body.yaw;
+            this.leftArm.yaw += this.body.yaw;
+            this.leftArm.pitch += this.body.yaw;
+            progress = 1.0F - this.handSwingProgress;
+            progress = progress * progress;
+            progress = progress * progress;
+            progress = 1.0F - progress;
+            float f2 = MathHelper.sin(progress * (float) Math.PI);
+            float f3 = MathHelper.sin(this.handSwingProgress * (float) Math.PI) * -(this.head.pitch - 0.7F) * 0.75F;
+            arm.pitch = (float) ((double) arm.pitch - ((double) f2 * 1.2D + (double) f3));
+            arm.yaw += this.body.yaw * 2.0F;
+            arm.roll += MathHelper.sin(this.handSwingProgress * (float) Math.PI) * -0.4F;
+        }
 
         if(entity.isUsingItem())
         {
@@ -100,5 +124,29 @@ public class GoblinTraderModel<G extends AbstractGoblinEntity> extends Composite
             this.rightLeg.yaw = (float) Math.toRadians(0);
             this.leftLeg.yaw = (float) Math.toRadians(0);
         }
+    }
+
+    @Override
+    public void setArmAngle(Arm arm, MatrixStack matrices)
+    {
+        switch(arm)
+        {
+            case LEFT -> {
+                this.leftArm.rotate(matrices);
+                matrices.translate(-0.235, -0.15, 0.25);
+                matrices.scale(0.75F, 0.75F, 0.75F);
+            }
+            case RIGHT -> {
+                this.rightArm.rotate(matrices);
+                matrices.translate(0.235, -0.15, 0.25);
+                matrices.scale(0.75F, 0.75F, 0.75F);
+            }
+        }
+    }
+
+
+    @Override
+    public ModelPart getHead() {
+        return this.head;
     }
 }
