dfeb4a88cc991ad04824b2de48d39aa1789e4d73
hat <flint9877@gmail.com>
2021-09-30 18:52:20 +0800

    Added Drops

--- src/main/java/net/hat/gt/init/ModEntities.java
+++ src/main/java/net/hat/gt/init/ModEntities.java
@@ -6,6 +6,7 @@ import net.hat.gt.entities.GoblinTraderEntity;
 import net.minecraft.entity.EntityDimensions;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.SpawnGroup;
+import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
@@ -14,7 +15,9 @@ public class ModEntities {
     public static final EntityType<GoblinTraderEntity> GOBLIN_TRADER = Registry.register(Registry.ENTITY_TYPE, new Identifier("goblintraders", "goblin_trader"),
             FabricEntityTypeBuilder.create(SpawnGroup.CREATURE, GoblinTraderEntity::new).dimensions(EntityDimensions.fixed(0.5f, 1.00f)).build()
     );
+
     public static void registerEntities() {
-        FabricDefaultAttributeRegistry.register(GOBLIN_TRADER, GoblinTraderEntity.createMobAttributes());
+        FabricDefaultAttributeRegistry.register(GOBLIN_TRADER, GoblinTraderEntity.createMobAttributes()
+                .add(EntityAttributes.GENERIC_MAX_HEALTH, 1.0D));
     }
 }
