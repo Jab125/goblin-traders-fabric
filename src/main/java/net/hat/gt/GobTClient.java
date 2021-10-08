c582019e8c803b8491dbe1f3819c1ed85c020209
hat <flint9877@gmail.com>
2021-10-08 11:03:06 +0800

    Fix to potential crash

--- src/main/java/net/hat/gt/GobTClient.java
+++ src/main/java/net/hat/gt/GobTClient.java
@@ -3,8 +3,9 @@ package net.hat.gt;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
-import net.fabricmc.fabric.api.client.rendereregistry.v1.EntityRendererRegistry;
+
 import net.fabricmc.fabric.api.client.rendering.v1.EntityModelLayerRegistry;
+import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
 import net.hat.gt.entities.GobinTraderRenderer;
 import net.hat.gt.entities.GoblinTraderModel;
 import net.hat.gt.entities.VeinGobinTraderRenderer;
@@ -14,13 +15,13 @@ import net.minecraft.util.Identifier;
 
 @Environment(EnvType.CLIENT)
 public class GobTClient implements ClientModInitializer {
-    public static final EntityModelLayer GOBLIN_MODEL_LAYER = new EntityModelLayer(new Identifier("goblintraders", "cube"), "goblin_render_layer");
-    public static final EntityModelLayer VEIN_GOBLIN_MODEL_LAYER = new EntityModelLayer(new Identifier("goblintraders", "cube"), "vein_goblin_render_layer");
+    public static final EntityModelLayer GOBLIN_MODEL_LAYER = new EntityModelLayer(new Identifier("goblintraders", "goblin"), "goblin_render_layer");
+    public static final EntityModelLayer VEIN_GOBLIN_MODEL_LAYER = new EntityModelLayer(new Identifier("goblintraders", "vgoblin"), "vein_goblin_render_layer");
     @Override
     public void onInitializeClient() {
         //EntityRenderer
-        EntityRendererRegistry.INSTANCE.register(ModEntities.GOBLIN_TRADER, GobinTraderRenderer::new);
-        EntityRendererRegistry.INSTANCE.register(ModEntities.VEIN_GOBLIN_TRADER, VeinGobinTraderRenderer::new);
+        EntityRendererRegistry.register(ModEntities.GOBLIN_TRADER, GobinTraderRenderer::new);
+        EntityRendererRegistry.register(ModEntities.VEIN_GOBLIN_TRADER, VeinGobinTraderRenderer::new);
         //EntityLayer
         EntityModelLayerRegistry.registerModelLayer(GOBLIN_MODEL_LAYER, GoblinTraderModel::getTexturedModelData);
         EntityModelLayerRegistry.registerModelLayer(VEIN_GOBLIN_MODEL_LAYER, GoblinTraderModel::getTexturedModelData);
