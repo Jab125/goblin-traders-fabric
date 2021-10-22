04c904aec48e6c1a1585628e7626d47c48e9c7d8
Jab125 <67534807+Jab125@users.noreply.github.com>
2021-10-22 15:44:49 +1100

    Fix easter egg not displaying

--- src/main/java/net/hat/gt/mixin/ItemMixin.java
+++ src/main/java/net/hat/gt/mixin/ItemMixin.java
@@ -12,6 +12,11 @@ import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 import java.util.List;
 
@@ -19,7 +24,8 @@ import java.util.List;
 public abstract class ItemMixin {
     @Shadow public abstract String getTranslationKey();
 
-    public void appendTooltip(ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext context) {
+    @Inject(method = "appendTooltip", at = @At(value = "HEAD"), locals = LocalCapture.CAPTURE_FAILSOFT)
+    public void appendTooltip(ItemStack stack, World world, List<Text> tooltip, TooltipContext context, CallbackInfo ci) {
         if (stack.isOf(Items.ACACIA_BOAT) && GobT.config.EASTER_EGGS) {
             tooltip.add(new TranslatableText(this.getTranslationKey() + ".desc").formatted(Formatting.GRAY));
         }
