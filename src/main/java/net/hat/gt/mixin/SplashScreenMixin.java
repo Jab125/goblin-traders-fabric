d2e1fde01780a7ff0041c44df933d4a1a40a12ce
Jab125 <67534807+Jab125@users.noreply.github.com>
2021-10-27 09:59:16 +1100

    Update SplashScreenMixin.java

--- src/main/java/net/hat/gt/mixin/SplashScreenMixin.java
+++ src/main/java/net/hat/gt/mixin/SplashScreenMixin.java
@@ -8,12 +8,14 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import static com.jab125.thonkutil.util.Util.isModInstalled;
+
 @Environment(EnvType.CLIENT)
 @Mixin(SplashTextResourceSupplier.class)
 public class SplashScreenMixin {
     @Inject(method = "get", at = @At(value = "RETURN"), cancellable = true)
     private void append(CallbackInfoReturnable<String> cir) {
-        if ((int) (Math.random() * 70) == 0) {
+        if ((int) (Math.random() * 70) == 0 && !isModInstalled("endgoblintraders")) {
             if ((int) (Math.random() * 2) == 0) {
                 cir.setReturnValue("Try End Goblin Traders for only $0.99!");
             } else {
