8edda32c17319cbf7622a4c28ddbdd38225c1f42
Jab125 <67534807+Jab125@users.noreply.github.com>
2021-10-27 13:45:02 +1100

    cape fix
    fix capes

--- src/main/java/net/hat/gt/misc/CapesLoader.java
+++ src/main/java/net/hat/gt/misc/CapesLoader.java
@@ -1,5 +1,6 @@
 package net.hat.gt.misc;
 
+import com.google.common.reflect.TypeToken;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import net.hat.gt.GobT;
@@ -7,12 +8,23 @@ import net.minecraft.util.Util;
 import org.apache.commons.io.IOUtils;
 
 import java.io.IOException;
+import java.lang.reflect.Type;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
-import java.util.Map;
+import java.util.HashMap;
+import java.util.List;
 
 public class CapesLoader {
-    public static Map PLAYERS;
+
+    public static class PlayerCapeData {
+        public String uuid;
+        public String cape;
+        public String name;
+    }
+
+    public static HashMap<String, String> UUID_CAPE_MAP = new HashMap<>();
+    private static final Type PLAYER_CAPE_DATA_TYPE = new TypeToken<List<PlayerCapeData>>() {
+    }.getType();
 
     public static void load() {
         Util.getMainWorkerExecutor().execute(() -> {
@@ -20,17 +32,20 @@ public class CapesLoader {
             Gson gson = new GsonBuilder().create();
             GobT.LOGGER.info("Loading capes data...");
             try {
-                PLAYERS = gson.fromJson(
+                List<PlayerCapeData> players = gson.fromJson(
                         IOUtils.toString(
                                 new URL("https://raw.githubusercontent.com/3e2j/goblin-traders-fabric/1.17/capes.json"),
                                 StandardCharsets.UTF_8
                         ),
-                        Map.class
+                        PLAYER_CAPE_DATA_TYPE
                 );
+                for (var player : players) {
+                    UUID_CAPE_MAP.put(player.uuid, player.cape);
+                }
             } catch (IOException e) {
                 GobT.LOGGER.warn("Failed to load capes.", e);
             }
-            GobT.LOGGER.info("Loaded capes for {} players. (Took {}ms)", PLAYERS.size(), System.currentTimeMillis()-startLoad);
+            GobT.LOGGER.info("Loaded capes for {} players. (Took {}ms)", UUID_CAPE_MAP.size(), System.currentTimeMillis() - startLoad);
         });
     }
 }
--- src/main/java/net/hat/gt/mixin/CapeMixin.java
+++ src/main/java/net/hat/gt/mixin/CapeMixin.java
@@ -1,13 +1,11 @@
 package net.hat.gt.mixin;
 
-import com.google.gson.JsonObject;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.hat.gt.GobT;
 import net.hat.gt.misc.CapesLoader;
 import net.minecraft.client.network.AbstractClientPlayerEntity;
 import net.minecraft.client.network.PlayerListEntry;
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.Identifier;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
@@ -16,30 +14,19 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.UUID;
-
-import static net.hat.gt.GobT.LOGGER;
-
 @Environment(EnvType.CLIENT)
 @Mixin(AbstractClientPlayerEntity.class)
 public abstract class CapeMixin {
-//    @Shadow @Nullable protected abstract PlayerListEntry getPlayerListEntry();
-//
-//    @Inject(method = "getCapeTexture", at = @At("RETURN"), cancellable = true)
-//    private void getCapeTexture_goblintraders(CallbackInfoReturnable<Identifier> info) {
-//        if (CapesLoader.PLAYERS != null) {
-//           JsonObject players = CapesLoader.PLAYERS.get("players");
-//            LOGGER.info(players.toString());
-//
-//            if (players.get("uuid").toString().equals(this.getPlayerListEntry().getProfile().getId().toString())) {
-//                info.setReturnValue(new Identifier(GobT.MODID, "textures/cape/cape_" + CapesLoader.PLAYERS.get(this.getPlayerListEntry().getProfile().getId()) + ".png"));
-//                LOGGER.info("YES CAPE");
-//            } else {
-//                LOGGER.info("NO CAPE");
-//            }
-//        }
-//    }
+    @Shadow
+    @Nullable
+    protected abstract PlayerListEntry getPlayerListEntry();
+
+    @Inject(method = "getCapeTexture", at = @At("RETURN"), cancellable = true)
+    private void getCapeTexture_goblintraders(CallbackInfoReturnable<Identifier> info) {
+        if (CapesLoader.UUID_CAPE_MAP != null) {
+            if (this.getPlayerListEntry() != null && CapesLoader.UUID_CAPE_MAP.containsKey(this.getPlayerListEntry().getProfile().getId().toString())) {
+                info.setReturnValue(new Identifier(GobT.MODID, "textures/cape/cape_" + CapesLoader.UUID_CAPE_MAP.get(this.getPlayerListEntry().getProfile().getId().toString()) + ".png"));
+            }
+        }
+    }
 }
