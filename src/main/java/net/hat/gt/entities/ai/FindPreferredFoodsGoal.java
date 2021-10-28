494cf9b10c4a99db2b39d73c21d31b475bef278b
Jab125 <67534807+Jab125@users.noreply.github.com>
2021-10-28 11:39:40 +1100

    Update FindPreferredFoodsGoal.java

--- src/main/java/net/hat/gt/entities/ai/FindPreferredFoodsGoal.java
+++ src/main/java/net/hat/gt/entities/ai/FindPreferredFoodsGoal.java
@@ -33,7 +33,7 @@ public class FindPreferredFoodsGoal extends Goal
     public boolean canStart()
     {
         this.findPreferredFoods();
-        return this.itemEntity != null && this.itemEntity.isAlive() && this.entity.getNavigation().findPathTo(this.itemEntity, 0) != null && (!this.itemEntity.isTouchingWater() || (this.itemEntity.isTouchingWater() && this.entity.canSwimToFood())) && this.entity.getInventory().canInsert(fakeItem) && this.entity.world.getGameRules().getBoolean(ModGameRules.GOBLIN_TRADERS_PICK_UP_FOODS);
+        return this.itemEntity != null && this.itemEntity.isAlive() && this.entity.getNavigation().findPathTo(this.itemEntity, 0) != null && (!this.itemEntity.isTouchingWater() || (this.itemEntity.isTouchingWater() && this.entity.canSwimToFood())) && (this.entity.getInventory().canInsert(fakeItem) || canInsertPartialStack(this.itemEntity.getStack().copy())) && this.entity.world.getGameRules().getBoolean(ModGameRules.GOBLIN_TRADERS_PICK_UP_FOODS);
     }
 
     @Override
@@ -45,11 +45,27 @@ public class FindPreferredFoodsGoal extends Goal
         if(path != null) this.entity.getNavigation().startMovingAlong(path, 0.4F);
         if(this.entity.distanceTo(this.itemEntity) <= 1.0D && this.itemEntity.isAlive())
         {
-            if (this.entity.getInventory().canInsert(fakeItem)) {
+            if (this.entity.getInventory().canInsert(fakeItem) && false) {
                 ItemStack item = this.itemEntity.getStack();
                 this.itemEntity.remove(Entity.RemovalReason.KILLED);
                 this.entity.addFoodToStorage(item);
                 this.entity.world.playSound(null, this.itemEntity.getX(), this.itemEntity.getY(), this.itemEntity.getZ(), SoundEvents.ENTITY_ITEM_PICKUP, SoundCategory.NEUTRAL, 1.0F, 0.75F);
+            } else {
+                ItemStack testItem = this.itemEntity.getStack().copy();
+                boolean isSuccessful = false;
+                for (int i = this.itemEntity.getStack().copy().getCount(); i > 0; i--) {
+                    testItem.setCount(1);
+                    if (this.entity.getInventory().canInsert(testItem)) {
+                        this.itemEntity.getStack().setCount(this.itemEntity.getStack().copy().getCount() - 1);
+                        this.entity.addFoodToStorage(testItem);
+                        isSuccessful = true;
+                    } else {
+                        if (isSuccessful) {
+                            this.entity.world.playSound(null, this.itemEntity.getX(), this.itemEntity.getY(), this.itemEntity.getZ(), SoundEvents.ENTITY_ITEM_PICKUP, SoundCategory.NEUTRAL, 1.0F, 0.75F);
+                        }
+                        break;
+                    }
+                }
             }
         }
     }
@@ -57,7 +73,7 @@ public class FindPreferredFoodsGoal extends Goal
     @Override
     public boolean shouldContinue()
     {
-        return this.itemEntity.isAlive() && this.entity.isAlive() && this.entity.getNavigation().findPathTo(this.itemEntity, 0) != null && (!this.itemEntity.isTouchingWater() || (this.itemEntity.isTouchingWater() && this.entity.canSwimToFood())) && this.entity.getInventory().canInsert(fakeItem) && this.entity.world.getGameRules().getBoolean(ModGameRules.GOBLIN_TRADERS_PICK_UP_FOODS);
+        return this.itemEntity.isAlive() && this.entity.isAlive() && this.entity.getNavigation().findPathTo(this.itemEntity, 0) != null && (!this.itemEntity.isTouchingWater() || (this.itemEntity.isTouchingWater() && this.entity.canSwimToFood())) && (this.entity.getInventory().canInsert(fakeItem) || canInsertPartialStack(this.itemEntity.getStack().copy())) && this.entity.world.getGameRules().getBoolean(ModGameRules.GOBLIN_TRADERS_PICK_UP_FOODS);
     }
 
     private void findPreferredFoods()
@@ -72,5 +88,15 @@ public class FindPreferredFoodsGoal extends Goal
             }
         }
 
+    }
+
+    private boolean canInsertPartialStack(ItemStack itemstack) {
+        ItemStack testItem = itemstack.copy();
+        for (int i = itemEntity.getStack().copy().getCount(); i > 0; i--) {
+            testItem.setCount(i);
+            if (this.entity.getInventory().canInsert(testItem))
+            return true;
+        }
+        return false;
     }
 }
