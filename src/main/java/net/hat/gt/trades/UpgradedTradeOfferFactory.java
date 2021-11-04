904578ced6e000b89f200ad7056838fed9c42720
Jab125 <67534807+Jab125@users.noreply.github.com>
2021-11-04 11:05:54 +1100

    Create UpgradedTradeOfferFactory.java

---
+++ src/main/java/net/hat/gt/trades/UpgradedTradeOfferFactory.java
@@ -0,0 +1,14 @@
+package net.hat.gt.trades;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.village.TradeOffer;
+import net.minecraft.village.TradeOffers;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Random;
+
+public interface UpgradedTradeOfferFactory extends TradeOffers.Factory {
+    @Nullable
+    @Override
+    UpgradedTradeOffer create(Entity entity, Random random);
+}
