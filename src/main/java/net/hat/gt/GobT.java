7fd80044c9d88a85b2cea9a5d0e6753bbbb9b7fa
Jab125 <67534807+Jab125@users.noreply.github.com>
2021-11-10 16:57:30 +1100

    it works

--- src/main/java/net/hat/gt/GobT.java
+++ src/main/java/net/hat/gt/GobT.java
@@ -6,8 +6,10 @@ import com.jab125.util.type.BasicTrade;
 import me.shedaniel.autoconfig.AutoConfig;
 import me.shedaniel.autoconfig.serializer.Toml4jConfigSerializer;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
 import net.hat.gt.config.GoblinTradersConfig;
 import net.hat.gt.init.*;
+import net.minecraft.resource.ResourceType;
 import net.minecraft.util.Identifier;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -26,6 +28,7 @@ public class GobT implements ModInitializer {
         TradeManager manager = TradeManager.instance();
         manager.registerTrader(ModEntities.GOBLIN_TRADER);
         manager.registerTrader(ModEntities.VEIN_GOBLIN_TRADER);
+        ResourceManagerHelper.get(ResourceType.SERVER_DATA).registerReloadListener(manager);
         manager.registerTypeSerializer(BasicTrade.SERIALIZER);
         ModGameRules.registerGameRules();
         ModSounds.registerSounds();
