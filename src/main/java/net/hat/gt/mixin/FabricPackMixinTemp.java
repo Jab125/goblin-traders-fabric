8b41f43e922910cf07e1272ee3bd65b4c3eb4f00
Jab125 <67534807+Jab125@users.noreply.github.com>
2021-11-14 17:35:49 +1100

    Custom name for vanilla-ish pack

---
+++ src/main/java/net/hat/gt/mixin/FabricPackMixinTemp.java
@@ -0,0 +1,21 @@
+package net.hat.gt.mixin;
+
+import net.fabricmc.fabric.impl.resource.loader.ResourceManagerHelperImpl;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+
+@Mixin(ResourceManagerHelperImpl.class)
+public class FabricPackMixinTemp {
+    @ModifyVariable(method = "registerBuiltinResourcePack", at = @At(value = "STORE" ,target = "Lnet/fabricmc/fabric/impl/resource/loader/ResourceManagerHelperImpl;registerBuiltinResourcePack(Lnet/minecraft/util/Identifier;Ljava/lang/String;Lnet/fabricmc/loader/api/ModContainer;Lnet/fabricmc/fabric/api/resource/ResourcePackActivationType;)Z"), index = 0, ordinal = 2, remap = false)
+    private static String changeName(String name) {
+        System.out.println("name: " + name);
+        if (name.equals("goblintraders/gobtvanillaish")) {
+            System.out.println("YES");
+            return "Vanilla-ish Trades";
+        } else {
+            System.out.println(name + " does not equal goblintraders/gobtvanillaish");
+        }
+        return name;
+    }
+}
