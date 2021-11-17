23229b736b79df1042ef17fbdfa08695a2337583
Jab125 <67534807+Jab125@users.noreply.github.com>
2021-11-17 12:21:23 +1100

    Minimum spawn height should be 0, not -64

--- src/main/java/net/hat/gt/config/GoblinTradersConfig.java
+++ src/main/java/net/hat/gt/config/GoblinTradersConfig.java
@@ -35,7 +35,7 @@ public class GoblinTradersConfig implements ConfigData {
         public int SPAWN_CHANCE = 25;
         public int SPAWN_DELAY = 24000;
         public int MAX_SPAWN_HEIGHT = 63;
-        public int MIN_SPAWN_HEIGHT = -64;
+        public int MIN_SPAWN_HEIGHT = 0;
     }
 
     public static class VeinGoblinTrader {
