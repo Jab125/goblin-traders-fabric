0c834a66f9573dc30825e500e07d9e598bed21b0
Jab125 <67534807+Jab125@users.noreply.github.com>
2021-11-18 17:14:53 +1100

    neat

--- src/main/java/net/hat/gt/datagen/DataGen.java
+++ src/main/java/net/hat/gt/datagen/DataGen.java
@@ -7,7 +7,7 @@ import java.io.File;
 import java.io.IOException;
 
 public class DataGen {
-    public static final boolean ENABLED = System.getProperty("fabric-api.datagen") != null;
+    public static final boolean ENABLED = System.getProperty("goblintraders.datagen") != null;
     public static void run() throws IOException {
         DataGenerator dataGenerator = new DataGenerator(new File("../src/main/generated/resources").toPath(), null);
         DataGeneraton.registerCommonProviders(dataGenerator);
