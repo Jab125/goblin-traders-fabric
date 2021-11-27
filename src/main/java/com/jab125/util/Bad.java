e1a81363e3a5e6a17de8a332338f278b5b73d27a
Jab125 <67534807+Jab125@users.noreply.github.com>
2021-11-28 10:56:04 +1100

    y e s

---
+++ src/main/java/com/jab125/util/Bad.java
@@ -0,0 +1,34 @@
+package com.jab125.util;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.*;
+import static java.lang.annotation.ElementType.TYPE;
+
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target(value={CONSTRUCTOR, FIELD, LOCAL_VARIABLE, METHOD, PACKAGE, MODULE, PARAMETER, TYPE})
+public @interface Bad {
+    /**
+     * Returns the version in which the annotated element became deprecated.
+     * The version string is in the same format and namespace as the value of
+     * the {@code @since} javadoc tag. The default value is the empty
+     * string.
+     *
+     * @return the version string
+     * @since 9
+     */
+    String since() default "";
+
+    /**
+     * Indicates whether the annotated element is subject to removal in a
+     * future version. The default value is {@code false}.
+     *
+     * @return whether the element is subject to removal
+     * @since 9
+     */
+    boolean forRemoval() default false;
+}
--- src/main/java/net/hat/gt/entities/GoblinTraderRenderer.java
+++ src/main/java/net/hat/gt/entities/GoblinTraderRenderer.java
@@ -1,7 +1,12 @@
 package net.hat.gt.entities;
 
+/**
+ * @deprecated use {@link net.hat.gt.entities.GobinTraderRenderer}
+ */
 // if you want a goblin trader renderer (a bad one) come here :P
 @SuppressWarnings("unused")
+@com.jab125.util.Bad(since = "1/1/1970")
+@Deprecated
 public class GoblinTraderRenderer extends GobinTraderRenderer {
     public GoblinTraderRenderer(net.minecraft.client.render.entity.EntityRendererFactory.Context context) {
         super(context);
