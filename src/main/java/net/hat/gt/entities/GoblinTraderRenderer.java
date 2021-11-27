ef4252dd5825d673b04bce47fe28ed919cab63f5
Jab125 <67534807+Jab125@users.noreply.github.com>
2021-11-27 20:48:17 +1100

    Added a GoblinTraderRenderer (no warranty if something goes wrong if you use it)

---
+++ src/main/java/net/hat/gt/entities/GoblinTraderRenderer.java
@@ -0,0 +1,9 @@
+package net.hat.gt.entities;
+
+// if you want a goblin trader renderer (a bad one) come here :P
+@SuppressWarnings("unused")
+public class GoblinTraderRenderer extends GobinTraderRenderer {
+    public GoblinTraderRenderer(net.minecraft.client.render.entity.EntityRendererFactory.Context context) {
+        super(context);
+    }
+}
